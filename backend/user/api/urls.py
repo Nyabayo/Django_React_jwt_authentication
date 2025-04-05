4a6d7904222080fa1645efc8aba9bf929a87c00f
Nyabayo <ernestosindo9@gmail.com>
2025-04-05 16:55:09 +0300

    frontend

--- backend/user/api/urls.py
+++ backend/user/api/urls.py
@@ -7,3 +7,4 @@ urlpatterns = [
     path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
